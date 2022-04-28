f8684656ad7a0c4f1f4591d6e95113eeb76555f6
hookkl21 <hookkl21@gmail.com>
2022-04-28 18:22:24 -0400

    debug start app

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ app.use((err, req, res, next) => {
   if (!err.message) err.message = "Something Went Wrong";
   res.status(status).render("error", { err });
 });
-
+const port = process.env.PORT || 3000;
 app.listen(3000, () => {
-  console.log("Listening port 3000");
+  console.log(`Listening port ${port}`);
 });
